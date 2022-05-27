b28dd49a3b85745df9a20f721424751c85cf3175
Hanxx <hanxx@Hanxxs-MacBook-Pro.local>
2022-05-27 14:19:09 +0800

    feat: remove some config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,3 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 source ~/.config/fish/alias.fish
-export PATH="$PATH:$HOME/.pnpm/bin"
--- dot_config/private_fish/functions/fish_greeting.fish
+++
@@ -1,3 +0,0 @@
-function fish_greeting
-  echo (__batman_color_gray)(uname -mnprs)(__batman_color_off)
-end

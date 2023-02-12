8d96083d904521a63802e041111c75cf25c4c953
hanxx <xsytby1112@gmail.com>
2023-02-12 20:20:30 +0800

    feat(nvim): add diffview

--- .config/nvim/lua/plugins/setup.lua
+++ .config/nvim/lua/plugins/setup.lua
@@ -56,6 +56,7 @@ return require('packer').startup(function(use)
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   use 'tpope/vim-surround'
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins

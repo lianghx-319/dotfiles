0bf3d5155a1a5ad56f3c34360e7c44f8e1a7e6d7
hanxx <xsytby1112@gmail.com>
2023-02-13 10:08:48 +0800

    fix(nvim): lsp named changed

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,13 +11,13 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   -- 确保安装，根据需要填写
   ensure_installed = {
-    "sumneko_lua",
+    "lua_ls",
   },
 })
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
   capabilities = capabilities,
   settings = {
     Lua = {

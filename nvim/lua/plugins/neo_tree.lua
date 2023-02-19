c3c259127e6deec6aea531f1fee7e20ad67b186c
hanxx <xsytby1112@gmail.com>
2023-02-19 20:22:55 +0800

    feat(nvim): neo-tree show dotfiles

--- nvim/lua/plugins/neo_tree.lua
+++ nvim/lua/plugins/neo_tree.lua
@@ -5,6 +5,16 @@ return {
       window = {
         position = "right",
       },
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            ".DS_Store",
+          },
+        },
+      },
     },
   },
 }

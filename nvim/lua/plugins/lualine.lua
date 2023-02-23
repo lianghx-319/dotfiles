c2461a5a435988a2849a2321711b73594e6b418f
hanxx <xsytby1112@gmail.com>
2023-02-24 00:08:59 +0800

    feat(nvim): lualine add file fullname

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -32,7 +32,7 @@ return {
           --   },
           -- },
           -- { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          -- { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
           -- stylua: ignore
           -- {
           --   function() return require("nvim-navic").get_location() end,

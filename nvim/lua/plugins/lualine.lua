a4105b8079b35f6b2645b4f6f518a7d1bd1e9407
hanxx <xsytby1112@gmail.com>
2023-02-24 00:13:19 +0800

    feat(nvim): lualine enable more info

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -22,16 +22,16 @@ return {
         lualine_a = { { "mode", icon = "" } },
         lualine_b = { { "branch", icon = "" } },
         lualine_c = {
-          -- {
-          --   "diagnostics",
-          --   symbols = {
-          --     error = icons.diagnostics.Error,
-          --     warn = icons.diagnostics.Warn,
-          --     info = icons.diagnostics.Info,
-          --     hint = icons.diagnostics.Hint,
-          --   },
-          -- },
-          -- { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
           -- stylua: ignore
           -- {

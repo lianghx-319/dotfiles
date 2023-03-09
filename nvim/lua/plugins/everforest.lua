4efc57055fa25d76d19e3b38ce908ae1c198ca39
hanxx <xsytby1112@gmail.com>
2023-03-10 07:14:34 +0800

    feat(nvim): update everforest configuration

--- nvim/lua/plugins/everforest.lua
+++ nvim/lua/plugins/everforest.lua
@@ -8,5 +8,10 @@ return {
     vim.g.everforest_background = "hard"
     vim.g.everforest_better_performance = 1
     vim.g.everforest_transparent_background = 1
+    vim.g.everforest_enable_italic = 1
+    vim.g.everforest_dim_inactive_windows = 1
+    vim.g.everforest_ui_contrast = 1
+    vim.g.everforest_diagnostic_text_highlight = 1
+    vim.g.everforest_diagnostic_virtual_text = 1
   end,
 }

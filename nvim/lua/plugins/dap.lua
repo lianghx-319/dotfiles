8b1f53c8de72f9658f4ab951e1bf978118f4ff1f
hanxx <xsytby1112@gmail.com>
2023-03-16 21:26:35 +0800

    feat(nvim): update nvim config

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,4 +1,10 @@
 return {
-  { "mfussenegger/nvim-dap", lazy = true },
+  { "mfussenegger/nvim-dap" },
   { "rcarriga/nvim-dap-ui", lazy = true, dependancies = { "mfussenegger/nvim-dap" } },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    lazy = true,
+    dependancies = { "mfussenegger/nvim-dap" },
+    opts = { automatic_setup = true },
+  },
 }
--- nvim/lua/plugins/diffview.lua
+++ nvim/lua/plugins/diffview.lua
@@ -1,4 +1,3 @@
 return {
   "sindrets/diffview.nvim",
-  lazy = true,
 }

ab40af4db763794fcda114aff54bcbb876bba274
hanxx <xsytby1112@gmail.com>
2023-04-03 10:06:06 +0800

    feat(nvim): add chatgpt

---
+++ nvim/lua/plugins/chatgpt.lua
@@ -0,0 +1,5 @@
+return {
+  "jackMort/ChatGPT.nvim",
+  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+  cmd = { "ChatGPT", "ChatGPTActAs", "ChatGPTEditWithInstructions" },
+}

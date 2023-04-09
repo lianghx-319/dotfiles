31d0c060ca7940d751161e5f1e27c4697f3609f1
hanxx <xsytby1112@gmail.com>
2023-04-09 14:57:30 +0800

    fix(nvim): dap breaking change

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,13 +1,16 @@
-return {
-  { "mfussenegger/nvim-dap" },
-  { "rcarriga/nvim-dap-ui", lazy = true, dependancies = { "mfussenegger/nvim-dap" } },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    dependancies = { "mfussenegger/nvim-dap" },
-    opts = { automatic_setup = true },
-    config = function(_, opts)
-      require("mason-nvim-dap").setup(opts)
-      require("mason-nvim-dap").setup_handlers({})
-    end,
-  },
-}
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+if true then return {} end
+
+--   { "mfussenegger/nvim-dap" },
+--   { "rcarriga/nvim-dap-ui", lazy = true, dependancies = { "mfussenegger/nvim-dap" } },
+--   {
+--     "jay-babu/mason-nvim-dap.nvim",
+--     dependancies = { "mfussenegger/nvim-dap" },
+--     opts = { automatic_setup = true },
+--     config = function(_, opts)
+--       require("mason-nvim-dap").setup(opts)
+--       require("mason-nvim-dap").setup_handlers({})
+--     end,
+--   },
+-- }

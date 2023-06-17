e19443f0d63bae48d75abeb9302a4bd7d9efcd74
hanxx <xsytby1112@gmail.com>
2023-06-17 17:19:24 +0800

    feat: fisher update

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,12 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Global bin PATH
+set -px --path PATH "/opt/homebrew/bin"
+set -px --path PATH "/usr/local/bin"
+set -px --path PATH "$HOME/.cargo/bin"
+
+# starship
 starship init fish | source
 
 # proxy
@@ -9,22 +15,11 @@ set proxy_host 192.168.2.4:7890
 set proxy_auth false
 set BAT_THEME "Catppuccin-macchiato"
 
-# starship
-# starship init fish | source
-
 # import all alias
 source ~/.config/fish/alias.fish
 
-# Rust
-set -px --path PATH "$HOME/.cargo/bin"
-
-# Load lvim
-set -px --path PATH "$HOME/.local/bin"
-
 # 1password
-set -px --path PATH "/usr/local/bin"
 op completion fish | source
-
 
 # pnpm
 set -gx PNPM_HOME "$HOME/Library/pnpm"

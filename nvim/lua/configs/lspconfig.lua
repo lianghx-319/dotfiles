4e183d50fa9732d4dea4033a613031a083b59e54
hanxx <xsytby1112@gmail.com>
2024-05-19 16:25:34 +0800

    feat(nvim): config lsp

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver", "clangd", "tailwindcss", "jsonls", "taplo" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do

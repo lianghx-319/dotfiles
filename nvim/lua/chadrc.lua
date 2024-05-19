71ec0b0c66644939c9ff839173a479c93fa1ee04
hanxx <xsytby1112@gmail.com>
2024-05-19 15:07:54 +0800

    feat(nvim): add starup dashboard

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -8,6 +8,33 @@ M.ui = {
 	theme = "everforest",
   transparency = true,
 
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      "                                                                       ",
+			"                                                                     ",
+			"       ████ ██████           █████      ██                     ",
+			"      ███████████             █████                             ",
+			"      █████████ ███████████████████ ███   ███████████   ",
+			"     █████████  ███    █████████████ █████ ██████████████   ",
+			"    █████████ ██████████ █████████ █████ █████ ████ █████   ",
+			"  ███████████ ███    ███ █████████ █████ █████ ████ █████  ",
+			" ██████  █████████████████████ ████ █████ █████ ████ ██████ ",
+			"                                                                       ",
+			"                                                                       ",
+			"                                                                       ",
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },

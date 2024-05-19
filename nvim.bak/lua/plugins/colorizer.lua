fcd0bf999ee1a903f547738a34627f00db47801a
hanxx <xsytby1112@gmail.com>
2024-05-19 14:47:49 +0800

    feat: remove old nvim

--- nvim.bak/lua/plugins/colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "norcalli/nvim-colorizer.lua",
-  lazy = true,
-  opts = {
-    "*",
-  },
-}
--- nvim.bak/lua/plugins/dracula.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "Mofiqul/dracula.nvim",
-  lazy = "true",
-  name = "dracula",
-  -- priority = 1000,
-  opts = {
-    transparent_bg = true,
-  },
-}

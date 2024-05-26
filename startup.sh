fd42a4e8f2894bd8bf0384cb311c5513a3323733
Hanxx <xsytby1112@gmail.com>
2024-05-26 21:14:45 +0800

    fix: link script

--- startup.sh
+++ startup.sh
@@ -8,7 +8,8 @@ symbol_link_config() {
     local name=$1
     local source=$dotfiles_location/$name
     local target=$config_loaction/$name
-    ln -sf $source $target
+    unlink $target
+    ln -s $source $target
 }
 
 configs=("fish" "alacritty" "tmux" "starship.toml" "nvim" "yabai" "skhd" "gunpg" "bat" "lazygit")

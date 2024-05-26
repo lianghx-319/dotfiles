fc41161cbb6d7e0b9b77ed57e96d16185de47060
Hanxx <xsytby1112@gmail.com>
2024-05-26 22:52:30 +0800

    feat(startup): fix lazygit link

--- startup.sh
+++ startup.sh
@@ -7,13 +7,17 @@ symbol_link_config() {
     local config_loaction=~/.config
     local name=$1
     local source=$dotfiles_location/$name
-    local target=$config_loaction/$name
+    local default_target=$2
+    local target=${default_target:="$config_loaction/$name"}
     unlink $target
     ln -s $source $target
 }
 
-configs=("fish" "alacritty" "tmux" "starship.toml" "nvim" "yabai" "skhd" "gunpg" "bat" "lazygit")
+configs=("fish" "alacritty" "tmux" "starship.toml" "nvim" "yabai" "skhd" "gunpg" "bat")
 
 for name in ${configs[@]}; do
     symbol_link_config $name
 done
+
+# FIXME: path with space not working
+symbol_link_config "lazygit/config.yml" "~/Library/Application\ Support/lazygit/config.yml"

e3cd72613d390d7743998c2587de72687894a3cf
Hanxx <xsytby1112@gmail.com>
2026-03-29 15:01:37 +0800

    feat: backup

--- fish/config.fish
+++ fish/config.fish
@@ -71,3 +71,7 @@ fish_add_path /Users/bytedance/.local/bin
 if test -f ~/.openclaw/completions/openclaw.fish
   source ~/.openclaw/completions/openclaw.fish
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
